7f53a04b69aa58fabb12b88f4449887240b0fa3a
webmrxu <webmrxu@foxmail.com>
2019-05-05 09:41:51 +0800

    block form init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* 公共css */
+@import url('./style/style.css');
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="clearfix">
+    <!-- 字段列表 -->
+    <div class="formitem-box">
+
+    </div>
+    <!-- 表单展示区 -->
+    <div class="form-box">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
+<style scoped>
+.formitem-box{
+  width: 50rem;
+  height: 500px;
+  float: left;
+  border: 1px solid #eee;
+}
+.form-box{
+  width: 50rem;
+  height: 500px;
+  float: left;
+  border: 1px solid #eee;
+}
+</style>

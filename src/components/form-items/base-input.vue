3302d3fb5e717a825547330f89feee259d86c59c
webmrxu <webmrxu@foxmail.com>
2019-05-06 15:39:21 +0800

    order file list

---
+++ src/components/form-items/base-input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-form-item label="活动名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+  </div>
+</template>
+<script>
+export default {
+  name: 'base-input',
+  data () {
+    return {
+      form: {}
+    }
+  }
+}
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,26 +1,47 @@
 <template>
-  <div class="clearfix">
+  <div class="clearfix home">
     <!-- 字段列表 -->
     <div class="formitem-box">
-
+      <div>
+        <span>输入框</span>
+      </div>
+      <div>
+        <span>数值输入</span>
+      </div>
     </div>
     <!-- 表单展示区 -->
     <div class="form-box">
-
+      <div>
+        <el-form ref="form" :model="form" label-width="80px">
+          <base-input/>
+        </el-form>
+      </div>
     </div>
     <!-- 字段自定义区 -->
     <div class="form-custom">
-      
     </div>
   </div>
 </template>
 <script>
+import BaseInput from '@/components/form-items/base-input'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    BaseInput
+  },
+  data () {
+    return {
+      form: {}
+    }
+  }
 }
 </script>
 <style scoped>
-.formitem-box{
+.home>div{
+  float: left;
+  width: 20%;
+}
+/* .formitem-box{
   width: 50rem;
   height: 500px;
   float: left;
@@ -31,5 +52,5 @@ export default {
   height: 500px;
   float: left;
   border: 1px solid #eee;
-}
+} */
 </style>

a1cedd72fa3dc43308fcf0f74abf142e79efb78d
unknown <webmrxu@foxmail.com>
2019-05-26 12:24:08 +0800

    添加代码高亮

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="clearfix home">
+    <div @click="test">test</div>
     <!-- 字段列表 -->
     <div class="formitem-box">
       <div>
@@ -28,10 +29,36 @@
           :rules="rules"
         />
       </div>
+      <div class="show-setting" @click="showSetting = true;">setting</div>
     </div>
     <!-- 字段自定义区 -->
-    <!-- <div class="form-custom">
-    </div>-->
+    <div>
+      <div v-if="showItemEdit" class="b-mask" @click="showItemEdit=false"></div>
+      <transition name="fade">
+        <div v-if="showItemEdit" class="form-custom b-form">
+          <div class="clearfix b-form-title">
+            <div class="float-l">输入组件修改</div> <div class="float-r bf-form-close" @click="showItemEdit=false">X</div>
+          </div>
+          <div>
+            hello world
+          </div>
+        </div>
+      </transition>
+    </div>
+    <!-- 展示积木表单配置 -->
+    <div>
+      <div v-if="showSetting" class="b-mask" @click="showSetting=false"></div>
+      <transition name="fade">
+        <div v-if="showSetting" class="form-custom b-form">
+          <div class="clearfix b-form-title">
+            <div class="float-l">展示积木表单配置</div> <div class="float-r bf-form-close" @click="showSetting=false">X</div>
+          </div>
+          <div>
+            <pre class="language-css"><code class="language-css">p { color: red }</code></pre>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 <script>
@@ -58,7 +85,9 @@ export default {
       },
       formBoxStyle: {
         marginLeft: "100px"
-      }
+      },
+      showItemEdit: false, // 展示组件配置
+      showSetting: false
     };
   },
   created() {
@@ -70,6 +99,9 @@ export default {
     window.onresize = utils.debounce(_this.dealWindowResize, 500);
   },
   methods: {
+    test() {
+      this.showItemEdit = !this.showItemEdit;
+    },
     dealWindowResize() {
       let windowWidth = window.document.documentElement.getBoundingClientRect()
         .width;
@@ -89,6 +121,23 @@ export default {
 };
 </script>
 <style scoped>
+.show-setting{
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 8px 15px;
+  cursor: pointer;
+}
+.bf-form-close{
+  cursor: pointer;
+}
+.float-r{
+  float: right;
+}
+.b-form-title{
+  padding: 15px;
+  border-bottom: 1px solid #e5e5e5;
+}
 .home > div {
   float: left;
   height: 500px;
@@ -111,6 +160,7 @@ export default {
   margin-bottom: 25px;
   padding-top: 20px;
   padding-right: 16px;
+  position: relative;
 }
 .formitem-box{
   margin-top: 8px;
@@ -125,5 +175,39 @@ export default {
   border: 1px solid #eee;
   padding: 10px 8px;
   cursor: pointer;
+}
+.form-custom{
+  position: fixed;
+  width: 600px;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: #fff;
+  z-index: 120;
+  border-left: 1px solid #eee;
+}
+.b-mask{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 110;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.4);
+}
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  width: 0px;
+}
+.float-l{
+  float: left;
+}
+.clearfix::after {
+    display: block;
+    clear: both;
+    content: "";
 }
 </style>

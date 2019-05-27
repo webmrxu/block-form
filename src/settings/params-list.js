40855e46b2f350bf5ef7e4cc9c95d96bd648e685
webmrxu <webmrxu@foxmail.com>
2019-05-27 10:34:50 +0800

    add params list file

---
+++ src/settings/params-list.js
@@ -0,0 +1,31 @@
+// 参数表
+// id      paramCode   paramValue     paramName     paramDes    desc
+//         参数code    参数值          参数名称       参数描述    排序
+const params = [
+  {
+    id: '1',
+    paramCode: 'code',
+    paramValue: 'val1',
+    paramName: '测试1'
+  },
+  {
+    id: '2',
+    paramCode: 'code',
+    paramValue: 'val2',
+    paramName: '测试3'
+  },
+  {
+    id: '3',
+    paramCode: 'code',
+    paramValue: 'val3',
+    paramName: '测试3'
+  },
+  {
+    id: '4',
+    paramCode: 'code',
+    paramValue: 'val4',
+    paramName: '测试5'
+  }
+]
+
+export default params;

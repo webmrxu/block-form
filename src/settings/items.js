84b209d9c63572c04dd1e57d153d3cd5b7fbe52f
xuguanping <xuguanping@foxmail.com>
2019-08-27 15:05:38 +0800

    配置修改

--- src/settings/items.js
+++ src/settings/items.js
@@ -2,7 +2,6 @@ const itemsSetting = [
   {
     type: 'input',
     placeholder: '请输入',
-    itemLayout: 'top-bottom', // top-bottom
     title: '文本框',
     field: 'userName',
     rulesId: ['1']
@@ -10,7 +9,6 @@ const itemsSetting = [
   {
     type: 'select',
     placeholder: '请输入',
-    itemLayout: 'left-right', // top-bottom
     title: '下拉选择框',
     field: 'parteents',
     paramCode: 'code',
@@ -29,7 +27,6 @@ const itemsSetting = [
   {
     type: 'email',
     placeholder: '请输入',
-    itemLayout: 'left-right', // top-bottom
     title: '邮箱',
     field: 'userEmail',
     rulesId: ['1', '2']

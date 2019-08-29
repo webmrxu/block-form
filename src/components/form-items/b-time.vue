9b91afe48b5ac6b7d58df28ea0e210203e8c93c9
xuguanping <xuguanping@foxmail.com>
2019-08-29 15:41:28 +0800

    添加多种表单输入

---
+++ src/components/form-items/b-time.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-form-item :label="item.title" :rules="item._rules" :prop="item.field" :label-width="item.labelWidth">
+      <el-input
+        v-model="itemValue"
+        :placeholder="item.placeholder"
+        @input="change"
+      ></el-input>
+    </el-form-item>
+  </div>
+</template>
+<script>
+export default {
+  name: "base-input",
+  props: ["item", "value"],
+  data() {
+    return {
+      itemValue: ''
+    }
+  },
+  watch: {
+    value(value) {
+      this.itemValue = value
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    change(value) {
+      this.$emit("update:value", value)
+    }
+  }
+}
+</script>

c64aba9ef8db29469e56dd009e25f891c63daa81
xuguanping <xuguanping@foxmail.com>
2019-09-17 10:41:54 +0800

    change npm publish version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import BlockForm from './components/block-form.vue';
 export default BlockForm;
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('block-form', BlockForm);
-}
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'block-form.min.js',
+    libraryTarget: 'umd',
     umdNamedDefine: true
   },
   optimization: {

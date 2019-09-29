a64000f0d616b716a40f9378398d2c2ae436a71d
xuguanping <xuguanping@foxmail.com>
2019-09-29 10:06:23 +0800

    添加测试页面

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
+import Test from '@/views/test'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })
---
+++ src/views/test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <block-form :itemSetting="setting" :formData="form" ref="formref"/>
+    <div>
+      <button @click="submit">submit</button> <button @click="validate">validate</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlockForm from '../components/block-form'
+export default {
+  name: "test",
+  components: {
+    BlockForm
+  },
+  methods: {
+    submit() {
+      console.log(this.form)
+    },
+    validate() {
+      this.$refs.formref.validate(res => {
+        console.log(res)
+      })
+    }
+  },
+  data() {
+    return {
+      form: {},
+      setting: [
+        {
+          field: 'title',
+          type: 'input',
+          title: '标题',
+          itemWidth: '60%',
+          rules: ['1']
+        },
+        {
+          field: 'placeholder',
+          type: 'count',
+          title: '日期',
+          itemWidth: '60%'
+        },
+        {
+          field: 'itemWidth',
+          type: 'email',
+          title: '邮箱',
+          itemWidth: '60%'
+        }
+      ]
+    }
+  }
+};
+</script>

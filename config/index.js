e701da4b1e28d672754e068ab2076413e9666e30
xuguanping <xuguanping@foxmail.com>
2019-10-09 14:22:21 +0800

    modify build config

--- config/index.js
+++ config/index.js
@@ -45,11 +45,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/b-form.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'b-form',
     assetsPublicPath: '/',
 
     /**
